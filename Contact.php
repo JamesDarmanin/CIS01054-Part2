3efbdb078a375f36aedd7b00aa649ef02eee36db
JamesDarmanin <119945719+JamesDarmanin@users.noreply.github.com>
2023-05-11 12:31:52 +0200

    Create Contact.php

---
+++ Contact.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('Templates');
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('Contact.html', []);
