ebac638243b2857b48b7282e352fb67eb41dbeba
JamesDarmanin <119945719+JamesDarmanin@users.noreply.github.com>
2023-05-29 18:46:07 +0200

    Create Favourite.php

---
+++ Favourite.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('Templates');
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('FavoriteDish.html', []);
+?>
