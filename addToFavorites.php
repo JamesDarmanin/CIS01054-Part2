ddf468dc7020a9a62716e7480376190231b368e5
JamesDarmanin <119945719+JamesDarmanin@users.noreply.github.com>
2023-05-29 18:45:23 +0200

    Create addToFavorites.php

---
+++ addToFavorites.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+// Function to store the favorited dish in the session
+function addToFavorites($dishName, $price)
+{
+    // Initialize the favorites array if it doesn't exist
+    if (!isset($_SESSION['favorites'])) {
+        $_SESSION['favorites'] = array();
+    }
+
+    // Add the favorited dish to the favorites array
+    $_SESSION['favorites'][] = array('dishName' => $dishName, 'price' => $price);
+}
+
+// Function to retrieve the favorited dishes from the session
+function getFavoriteDishes()
+{
+    // Return the favorites array if it exists, otherwise return an empty array
+    return isset($_SESSION['favorites']) ? $_SESSION['favorites'] : array();
+}
+
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the dish details from the form
+    $dishName = $_POST['food'];
+    $price = $_POST['price'];
+
+    // Call the addToFavorites function to store the favorited dish
+    addToFavorites($dishName, $price);
+}
+
+// Redirect back to the previous page
+$referer = $_SERVER['HTTP_REFERER'];
+header("Location: $referer");
+exit;
+?>
