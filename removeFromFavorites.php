96d3bcad3051028467ce5370ececbfd8d1912385
JamesDarmanin <119945719+JamesDarmanin@users.noreply.github.com>
2023-05-29 18:46:47 +0200

    Create removeFromFavorites.php

---
+++ removeFromFavorites.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $dishName = $_POST['dishName'];
+
+    // Remove the dish from the favorites list
+    removeFavoriteDish($dishName);
+}
+
+// Function to remove a dish from the favorites list
+function removeFavoriteDish($dishName)
+{
+    if (isset($_SESSION['favorites'])) {
+        // Loop through the favorites array and remove the dish with matching name
+        foreach ($_SESSION['favorites'] as $key => $favorite) {
+            if ($favorite['dishName'] === $dishName) {
+                unset($_SESSION['favorites'][$key]);
+                break;
+            }
+        }
+
+        // Reset the array keys after removing the dish
+        $_SESSION['favorites'] = array_values($_SESSION['favorites']);
+    }
+}
+
+// Redirect back to the favorites page
+header("Location: FavoriteDish.html");
+exit;
+?>
